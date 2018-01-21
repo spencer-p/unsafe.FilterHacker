265f5e2773239c4b89c9d131f1e4c5d0d60fb85f
Spencer Peterson <spencerjpeterson@gmail.com>
2018-01-20 23:09:30 -0800

    "initial"
    or: how i learned to stop worrying and upload to vcs

---
+++ db.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	DBURL = "localhost:27017"
+	DBNAME = "test"
+	COLLECTION = "posts"
+)
+
+type Post struct {
+	Id    bson.ObjectId `bson:"_id,omitempty"`
+	Image []byte        `bson:"image"`
+	Code  string        `bson:"code"`
+	Date  int           `bson:"date"`
+}
+
+func open() (*mgo.Session, error) {
+	session, err := mgo.Dial(DBURL)
+	if err != nil {
+		return nil, err
+	}
+	return session, nil
+}
+
+func uploadImage(img []byte) (bson.ObjectId, error) {
+	s, err := open()
+	if err != nil {
+		return "", err
+	}
+	defer s.Close()
+
+	// Make a new id
+	i := bson.NewObjectId()
+
+	// Insert the image into the posts collection
+	c := s.DB(DBNAME).C(COLLECTION)
+	err = c.Insert(&Post{Id: i, Image: img})
+	if err != nil {
+		return "", err
+	}
+
+	log.Print("Successfully inserted ", i.Hex())
+
+	return i, nil
+}
+
+func getImage(id string) ([]byte, error) {
+	s, err := open()
+	if err != nil {
+		return nil, err
+	}
+	defer s.Close()
+
+	c := s.DB(DBNAME).C(COLLECTION)
+
+	var result Post
+	err = c.Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Image, nil
+}
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type HandlePathData struct {
+	Path         string
+	TemplateFile string
+}
+
+const (
+	UPLOADPATH = "/new/"
+	EDITPATH   = "/edit/"
+	VIEWPATH   = "/view/"
+	IMAGEPATH = "/img/"
+)
+
+var templates = template.Must(template.ParseFiles(
+	"templates/new.html",
+	"templates/edit.html",
+	"templates/view.html",
+))
+
+// BUG(spencer): Users should not be able to POST edits to other's photos.
+
+// newHandler serves the page where users upload their initial photo. It then
+// redirects them to the editing page where they can finish their creation.
+func newHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		err := templates.ExecuteTemplate(w, "new.html", "fake_token")
+		if err != nil {
+			fmt.Fprintf(w, "An error occurred. TODO: 500 this")
+			log.Print("Error: ", err)
+		}
+	} else if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			log.Println("Could not get form file:", err)
+			return
+		}
+		defer file.Close()
+
+		// TODO make sure that it's an image (handler.Header)
+		_ = handler
+		bytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Println("Could not read the image:", err)
+			return
+		}
+
+		id, err := uploadImage(bytes)
+		if err != nil {
+			log.Println("Could not upload image:", err)
+			return
+		}
+
+		http.Redirect(w, r, EDITPATH+id.Hex(), http.StatusFound)
+	}
+}
+
+// editHandler serves the page on which users write code to filter the image they
+// previously uploaded. It allows them to submit code matching their image.
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len(EDITPATH):]
+	err := templates.ExecuteTemplate(w, "edit.html", idStr)
+	if err != nil {
+		fmt.Fprintf(w, "An error occurred. TODO: 500 this")
+		log.Print("Error: ", err)
+	}
+}
+
+// viewHandler displays a finished image+code combo.
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "view.html", nil)
+	if err != nil {
+		fmt.Fprintf(w, "An error occurred. TODO: 500 this")
+		log.Print("Error: ", err)
+	}
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len(IMAGEPATH):]
+	img, err := getImage(idStr)
+	if err != nil {
+		log.Println("Could not fetch image:", err)
+	}
+	w.Write(img)
+}
+
+func main() {
+	uploadImage(nil)
+
+	// Register handlers
+	http.HandleFunc(UPLOADPATH, newHandler)
+	http.HandleFunc(EDITPATH, editHandler)
+	http.HandleFunc(VIEWPATH, viewHandler)
+	http.HandleFunc(IMAGEPATH, imageHandler)
+
+	// Serve forever
+	http.ListenAndServe(":8000", nil)
+}
